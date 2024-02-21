1052ac7748dbdc3dc145243005261ee18a5bcc24
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-02-21 14:18:14 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -159,31 +159,16 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
-        """Generates a password reset token for a user.
-
-        Args:
-            email (str): A string representing the email address of the user to
-            generate a password reset token for.
-
-        Raises:
-            ValueError: If no user with the specified email address is found.
-
-        Returns:
-            str: A string representing the password reset token generated for
-            the user.
-        """
-        # Find the user with the specified email address
+        """Generates a reset password token if user exists"""
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            user = None
-        # If no user is found with specified email address, raise a ValueError
-        if user is None:
-            raise ValueError()
-        # Generate a new password reset token & update the user's record in db
+            raise ValueError
+
         reset_token = _generate_uuid()
+
         self._db.update_user(user.id, reset_token=reset_token)
-        # Return the generated password reset token
+
         return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
